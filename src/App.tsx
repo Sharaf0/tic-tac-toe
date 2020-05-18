f6658f852a1d991a2bd6ecce3a4a1380fe229b80
Sharaf0 <mohammedsharaf@live.de>
2020-05-18 20:05:48 +0200

    Added the general structure - Added node-sass - Added a style file (yes it is only one file for everything) - Added 5 * 5 game board

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+import classes from "./styles.module.scss";
 
 function App() {
-  return <></>;
+  return <div className={classes.game}>
+    <div className={classes.board}>
+      <div className={classes.boardRow}>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+      </div>
+      <div className={classes.boardRow}>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+      </div>
+      <div className={classes.boardRow}>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+      </div>
+      <div className={classes.boardRow}>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+      </div>
+      <div className={classes.boardRow}>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+        <button className={classes.square}></button>
+      </div>
+    </div>
+  </div>;
 }
 
 export default App;
